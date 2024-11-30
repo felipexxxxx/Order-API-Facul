3d7046a1b30a8477b98b1b01f48afb7dae3b04a6
Felipe Lins <felipeplinsesilva@gmail.com>
2024-11-29 21:13:18 -0300

    p minusculo

--- src/main/java/com/trabalhoArquitetura/trabalho/Controller/PedidoController.java
+++ src/main/java/com/trabalhoArquitetura/trabalho/Controller/PedidoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Pedido")
+@RequestMapping("/pedido")
 public class PedidoController {
 
     @Autowired
